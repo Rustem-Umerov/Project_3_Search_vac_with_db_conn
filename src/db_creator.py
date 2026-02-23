e2eae62ae58724d34d3bf466d5e60bf80ec0b0eb
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-02-23 20:42:41 +0300

    In the db_creator.py module, I wrote the following function: * create_database() - creates a new PostgreSQL database.

--- src/db_creator.py
+++ src/db_creator.py
@@ -1,7 +1,8 @@
-from typing import TypedDict
+from typing import Optional, TypedDict
 
 import psycopg2
 from psycopg2.extensions import connection, cursor
+from psycopg2.sql import SQL, Identifier
 
 from src.config import settings
 from src.logger_setup import get_logger
@@ -20,7 +21,12 @@ class DbParams(TypedDict):
 
 
 def connect_to_server() -> tuple[connection, cursor]:
-    """Подключение к системной базе PostgreSQL (postgres) для административных операций."""
+    """
+    Подключение к системной базе PostgreSQL (postgres) для административных операций.
+
+    :raises Exception: Любая ошибка при создании базы данных.
+    :return: (connection, cursor)
+    """
 
     try:
         params: DbParams = {
@@ -55,6 +61,7 @@ def database_exists(*, cur: cursor, dbname: str) -> bool:
     Проверяет, существует ли база данных с указанным именем.
     :param cur: Курсор psycopg2, используемый для выполнения SQL‑запросов.
     :param dbname: Имя базы данных
+    :raises Exception: Любая ошибка при создании базы данных.
     :return: True/False
     """
 
@@ -73,3 +80,34 @@ def database_exists(*, cur: cursor, dbname: str) -> bool:
     except Exception as e:
         logger.error("Ошибка при проверке существования базы %s: %s", dbname, e)
         raise
+
+
+def create_database(dbname: str) -> None:
+    """
+    Создаёт новую базу данных PostgreSQL.
+
+    :param dbname: Имя базы данных, которую нужно создать.
+    :raises Exception: Любая ошибка при создании базы данных.
+    """
+
+    conn: Optional[connection] = None
+    cur: Optional[cursor] = None
+
+    try:
+        # Подключаемся к системной базе postgres
+        conn, cur = connect_to_server()
+        conn.autocommit = True
+
+        logger.info("Создаём базу %s", dbname)
+        cur.execute(SQL("CREATE DATABASE {}").format(Identifier(dbname)))
+        logger.info("База %s успешно создана", dbname)
+
+    except Exception as e:
+        logger.error("Ошибка при создании базы %s: %s", dbname, e)
+        raise
+
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
